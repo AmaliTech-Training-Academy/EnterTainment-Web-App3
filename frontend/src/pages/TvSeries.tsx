7cadcaf42ef86cc6d7b4bd5955627b53bf940f90
kwabenakay <121056440+kayjob@users.noreply.github.com>
2023-04-13 19:38:01 +0000

    TV-Series: Added padding to the tablet and desktop view

--- frontend/src/pages/TvSeries.tsx
+++ frontend/src/pages/TvSeries.tsx
@@ -26,7 +26,7 @@ export default function TvSeries() {
 
 
   return (
-    <div className=' bg-teal-blue max-w-[1440px] mx-auto min-h-screen text-white flex flex-col gap-6 items-stretch px-4' >
+    <div className=' bg-teal-blue max-w-[1440px] mx-auto min-h-screen text-white flex flex-col gap-6 items-stretch px-4 tablet:px-6 mini-pc:px-12' >
         <Filter searchType='TV series' searchValue={search} setSearch={setSearch}/>
         <div  className=' text-xl text-left'>TV Series</div>
         <div className=' grid grid-cols-2 gap-4 tablet:grid-cols-3 mini-pc:grid-cols-4'>
@@ -48,7 +48,7 @@ export default function TvSeries() {
                                 <span className='flex gap-1'>
                                     <span>&bull;</span>
                                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path opacity="0.75" fill-rule="evenodd" clip-rule="evenodd" d="M5.448 2.68865H12V12H0V2.68865H2.952L1.332 0.72163L2.268 0.0174588L4.2 2.3453L6.132 0L7.068 0.72163L5.448 2.68865ZM1.2 3.85257V10.8361H7.2V3.85257H1.2ZM10.2 8.50824H9V7.34433H10.2V8.50824ZM9 6.18041H10.2V5.01649H9V6.18041Z" fill="white"/>
+                                        <path opacity="0.75" fillRule="evenodd" clipRule="evenodd" d="M5.448 2.68865H12V12H0V2.68865H2.952L1.332 0.72163L2.268 0.0174588L4.2 2.3453L6.132 0L7.068 0.72163L5.448 2.68865ZM1.2 3.85257V10.8361H7.2V3.85257H1.2ZM10.2 8.50824H9V7.34433H10.2V8.50824ZM9 6.18041H10.2V5.01649H9V6.18041Z" fill="white"/>
                                     </svg>
                                     {movie.category}
                                 </span>
