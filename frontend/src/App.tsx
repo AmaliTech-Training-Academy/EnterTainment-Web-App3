9bccce859d7e604480682013941baa2cfc5bcfe9
kwabenakay <121056440+kayjob@users.noreply.github.com>
2023-04-13 19:06:52 +0000

    TV-Series: Duplicated the movies page and changed the filter to movies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,12 @@ import './App.css';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Movies from './pages/Movies';
+import TvSeries from './pages/TvSeries';
 function App() {
   return (
-    <div className="App">
-      <Movies/>
-      {/* <header className="App-header">
+    <div className="App bg-teal-blue">
+      <TvSeries/>
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -21,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
---
+++ frontend/src/pages/TvSeries.tsx
@@ -0,0 +1,59 @@
+import React,{useEffect, useState} from 'react'
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import BookmarkOutlinedIcon from '@mui/icons-material/BookmarkOutlined';
+import rawData from '../data.json' 
+import Filter from '../components/Filter'
+import { movie } from '../dataTypes';
+export default function TvSeries() {
+    
+    const [search, setSearch] = useState<string>('')
+    const [data, setData] = useState(rawData)
+
+    useEffect(()=>{
+        setData(data.filter((movie)=>(movie.category==='TV Series')))
+    },[])
+
+
+    function toggleBookmark(movie:movie) {
+        let ind = data.indexOf(movie)
+        data[ind].isBookmarked=!data[ind].isBookmarked
+        setData([...data])
+    }
+
+    function filter (){
+        return data.filter((movie)=>movie.title.toLowerCase().includes(search.toLowerCase()))
+    }
+
+
+  return (
+    <div className=' bg-teal-blue max-w-[1440px] mx-auto min-h-screen text-white flex flex-col gap-6 items-stretch px-4' >
+        <Filter searchType='TV series' searchValue={search} setSearch={setSearch}/>
+        <div  className=' text-xl text-left'>TV Series</div>
+        <div className=' grid grid-cols-2 gap-4 tablet:grid-cols-3 mini-pc:grid-cols-4'>
+            {
+                filter().map((movie,ind)=>(
+                    <div key={movie.title+ind} className=' flex flex-col gap-2'>
+                        <div className=' mt-[-24px]'>
+                            <div className=' relative top-[27%] left-[77%] flex justify-center items-center w-8 h-8 bg-[rgba(0,0,0,.5)] hover:cursor-pointer rounded-full tablet:top-[27%] tablet:left-[79%]'
+                            onClick={()=>{
+                               toggleBookmark(movie)
+                            }}>
+                                {(movie.isBookmarked)?(<BookmarkOutlinedIcon/>):(<BookmarkBorderOutlinedIcon/>)}
+                            </div>
+                            <img  className=' rounded-lg' src={movie.thumbnail.regular.small} alt={movie.title} />
+                        </div>
+                        <div>
+                            <div className=' self-start flex gap-3 text-xs font-light'>
+                                <span>{movie.year}</span>
+                                <span>{movie.category}</span>
+                                <span>{movie.rating}</span>
+                            </div>
+                            <div className=' text-left text-sm font-medium'>{movie.title}</div>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
+  )
+}
